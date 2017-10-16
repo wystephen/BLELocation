c50b917f3d8590a5b8fa3146f3d640e7cab38ea7
steve <551619855@qq.com>
2017-10-16 19:25:21 +0800

    Initial commit

---
+++ DataPreprocess.py
@@ -0,0 +1,76 @@
+# -*- coding:utf-8 -*-
+# Created by steve @ 17-10-16 下午2:44
+'''
+                   _ooOoo_ 
+                  o8888888o 
+                  88" . "88 
+                  (| -_- |) 
+                  O\  =  /O 
+               ____/`---'\____ 
+             .'  \\|     |//  `. 
+            /  \\|||  :  |||//  \ 
+           /  _||||| -:- |||||-  \ 
+           |   | \\\  -  /// |   | 
+           | \_|  ''\---/''  |   | 
+           \  .-\__  `-`  ___/-. / 
+         ___`. .'  /--.--\  `. . __ 
+      ."" '<  `.___\_<|>_/___.'  >'"". 
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | | 
+     \  \ `-.   \_ __\ /__ _/   .-` /  / 
+======`-.____`-.___\_____/___.-`____.-'====== 
+                   `=---=' 
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
+         佛祖保佑       永无BUG 
+'''
+
+
+import matplotlib.pyplot as  plt
+
+import numpy as np
+import scipy as sp
+import pandas
+
+from TimeStampConvert import timestampconvert
+
+import os
+
+if __name__ == '__main__':
+
+    dir_name = '/home/steve/Data/BLELocation/1/'
+
+    the_file_list = list()
+
+    for f in os.listdir(dir_name):
+        if 'txt' in f:
+            tmp_file = open(dir_name + f)
+            the_file_list = tmp_file.readlines()
+
+    print(len(the_file_list))
+
+    print(len(the_file_list[1].split(',')))
+
+    all_data = np.zeros([len(the_file_list)-1,len(the_file_list)-1])
+
+    for i in range(1,len(the_file_list)):
+        tmp_str = the_file_list[i]
+        all_data[i,0] = timestampconvert(tmp_str.split(',')[0])
+        for j in range(1,len(tmp_str.split(','))-1):
+            all_data[i,j] = float(tmp_str.split(',')[j])
+
+
+    ### time,x,y, rssi0,rssi1 .... rssi n
+    plt.figure()
+    plt.plot(all_data[:,0])
+    plt.grid()
+
+    plt.figure()
+    plt.plot(all_data[:,1],all_data[:,2])
+    plt.grid()
+
+    plt.figure()
+    for i in range(3,all_data.shape[1]):
+        plt.plot(all_data[:,i],'.-')
+    plt.show()
+
+
+
---
+++ TimeStampConvert.py
@@ -0,0 +1,53 @@
+# -*- coding:utf-8 -*-
+# Created by steve @ 17-10-16 下午2:46
+'''
+                   _ooOoo_ 
+                  o8888888o 
+                  88" . "88 
+                  (| -_- |) 
+                  O\  =  /O 
+               ____/`---'\____ 
+             .'  \\|     |//  `. 
+            /  \\|||  :  |||//  \ 
+           /  _||||| -:- |||||-  \ 
+           |   | \\\  -  /// |   | 
+           | \_|  ''\---/''  |   | 
+           \  .-\__  `-`  ___/-. / 
+         ___`. .'  /--.--\  `. . __ 
+      ."" '<  `.___\_<|>_/___.'  >'"". 
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | | 
+     \  \ `-.   \_ __\ /__ _/   .-` /  / 
+======`-.____`-.___\_____/___.-`____.-'====== 
+                   `=---=' 
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
+         佛祖保佑       永无BUG 
+'''
+
+
+
+import time
+
+
+def timestampconvert(str='10-04-31-73'):
+    the_time = 0.0
+    for i in range(3):
+        # print(i)
+        the_time = the_time*60.0 + float(str.split('-')[i])
+        if(i>0 and float(str.split('-')[i])>60.0):
+            print('error at timestampconvert :',str,'some value is out of range')
+
+
+    the_time += float(str.split('-')[3]) / 1000.0
+    return the_time
+
+
+if __name__ == '__main__':
+    print('begin test')
+
+    print('one hour',timestampconvert('1-0-0-0')-timestampconvert('0-0-0-0'))
+    print('one min', timestampconvert('0-1-0-0'))
+    print('one sec',timestampconvert('0-0-1-0'))
+    print('100 ms',timestampconvert('0-0-0-100'))
+    print('not out range',timestampconvert('1000'))
+
+
