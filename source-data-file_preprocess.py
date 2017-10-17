184c22ea79a1ff32015c1435838f3aa9adba611a
steve <551619855@qq.com>
2017-10-17 19:39:05 +0800

    ~

--- source-data-file_preprocess.py
+++ source-data-file_preprocess.py
@@ -60,37 +60,41 @@ if __name__ == '__main__':
         for j in range(distance_matrix.shape[1]):
             distance_matrix[i, j] = np.linalg.norm(data[i, 1:] - data[j, 1:])
 
-    distance_matrix = distance_matrix / np.linalg.norm(distance_matrix,axis=1)
+    # distance_matrix = distance_matrix / np.linalg.norm(distance_matrix, axis=1)
     plt.figure()
     plt.title('all distance ')
     plt.imshow(distance_matrix)
+    print(data)
 
     # add
-    initial_val = data[0,1:]
+    initial_val = data[0, 1:]*1.0
     initial_time = np.zeros_like(initial_val)
-    initial_time += data[0,0]
+    initial_time += data[0, 0]
 
     new_data = np.zeros_like(data)
 
     for i in range(data.shape[0]):
 
         # updata intiial_val
-        for j in range(1,data.shape[1]):
-            if data[i,j] < 0.0:
-                initial_val[j-1] = data[i,j]
-                initial_time[j-1] = data[i,0]
-
+        for j in range(1, data.shape[1]):
+            if data[i, j] < 0.0:
+                initial_val[j - 1] = data[i, j]
+                initial_time[j - 1] = data[i, 0]
 
         # clear initial val
-        for j in range(1,data.shape[1]):
-            if data[i,0] - initial_val[j-1] > 2.0:
-                initial_val = 0.0
+        for j in range(1, data.shape[1]):
+            if data[i, 0] - initial_time[j - 1] > 2.0:
+                initial_val[j-1] = 0.0
 
-        new_data[i,0] = data[i,0] * 1.0
-        new_data[i,1:] = initial_val * 1.0
+        new_data[i, 0] = data[i, 0] * 1.0
+        new_data[i, 1:] = initial_val * 1.0
+        print(i,':')
+        print(data[i,1:])
+        print(new_data[i,1:])
 
+    data = new_data * 1.0
 
-    data = new_data
+    print(data)
 
     plt.figure()
     plt.title('all new data')
@@ -102,15 +106,9 @@ if __name__ == '__main__':
         for j in range(distance_matrix.shape[1]):
             distance_matrix[i, j] = np.linalg.norm(data[i, 1:] - data[j, 1:])
 
-    distance_matrix = distance_matrix / np.linalg.norm(distance_matrix,axis=1)
+    # distance_matrix = distance_matrix / np.linalg.norm(distance_matrix, axis=1)
     plt.figure()
     plt.title('all new distance ')
     plt.imshow(distance_matrix)
-
-
-
-
-
-
 
     plt.show()
